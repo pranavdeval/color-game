fba6797d9631d2b167e0fb7071df3e0852461a64
pranav <pranavdeval@gmail.com>
2020-01-29 01:05:09 +0530

    fix in app.listen()

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.get("/",function(req,res) {
     res.render("colorgameproject");
 });
 
-app.listen(3000,"127.0.0.1",function() {
+app.listen(process.env.PORT,process.env.IP,function() {
     console.log("server has started...");
 });
